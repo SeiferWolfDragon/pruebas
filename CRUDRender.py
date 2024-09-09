d761e466bffd3bee62d06d05f902e0901ec7b622
SeiferWolfDragon <diego.vazquez.1994@hotmail.com>
2024-09-09 16:35:13 -0600

    se ha instalado gunicorn, y modificado Crudrender.py

--- CRUDRender.py
+++ CRUDRender.py
@@ -82,4 +82,5 @@ def delete(id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Usar el puerto de Render
+    app.run(host='0.0.0.0', port=port)
